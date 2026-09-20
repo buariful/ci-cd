d826a5c06572c1380307cc50e9d2e3ae88368cd9
ariful <arifulislam321054@gmail.com>
2026-09-20 21:05:17 +0600

    mssge updated

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
-  res.send("API is running...");
+  res.send("API is running smoothly...");
 });
 
 app.get('/health', (req, res) => {
