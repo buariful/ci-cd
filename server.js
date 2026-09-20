0b99522723c25d45fc50eec02cf51b92c7d592f8
ariful <arifulislam321054@gmail.com>
2026-09-20 20:55:43 +0600

    added ci/cd workflows

--- server.js
+++ server.js
@@ -19,6 +19,14 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
+app.get('/health', (req, res) => {
+  res.status(200).json({ 
+    status: 'OK', 
+    timestamp: new Date(),
+    mongodb: mongoose.connection.readyState === 1 ? 'connected' : 'disconnected'
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
